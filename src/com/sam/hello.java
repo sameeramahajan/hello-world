a5e3129916195549e8bc79b3ccb53f9454e840df
sameeramahajan <sameera.mahajan.85@gmail.com>
2019-06-06 18:44:18 +0530

    add to new branch

--- src/com/sam/hello.java
+++ src/com/sam/hello.java
@@ -2,7 +2,6 @@ package com.sam;
 
 public class hello {
 	public static void main (String[] args){
-		int a=5;
-		System.out.println("Hello Sameera!!!!!!!!!!!! --- "+a);
+		System.out.println("Hello World!!!");
 	}
 }
