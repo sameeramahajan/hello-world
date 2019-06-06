1ccc05ccd1dba5f9f9cccdd85377e0e7995ec3fc
sameeramahajan <sameera.mahajan.85@gmail.com>
2019-06-06 19:53:42 +0530

    add new line

--- src/com/sam/hello.java
+++ src/com/sam/hello.java
@@ -2,7 +2,7 @@ package com.sam;
 
 public class hello {
 	public static void main (String[] args){
-		
 		System.out.println("Hello Sameera!!!!!!!!!!!!");
+		System.out.println("Adding a new line");
 	}
 }
