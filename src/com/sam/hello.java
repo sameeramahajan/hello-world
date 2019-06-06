f0762c8897ab21ce5c77523011db2219ebbd14c1
sameeramahajan <sameera.mahajan.85@gmail.com>
2019-06-06 19:51:05 +0530

    resolving conflict

--- src/com/sam/hello.java
+++ src/com/sam/hello.java
@@ -2,11 +2,6 @@ package com.sam;
 
 public class hello {
 	public static void main (String[] args){
-<<<<<<< HEAD
-		System.out.println("Hello World!!!");
-=======
-		
-		System.out.println("Hello World!!!");
->>>>>>> origin/feature1_branch
+		System.out.println("Hello Sameera!!!!!!!!!!!!");
 	}
 }
